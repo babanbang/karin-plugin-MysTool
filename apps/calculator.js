b8a3e92e732617d9ada91c087a5afbc84fc72b96
bbaban <zasd32345@gmail.com>
2024-06-03 22:32:45 +0800

    优化养成计算前置处理

--- apps/calculator.js
+++ apps/calculator.js
@@ -14,7 +14,7 @@ export class calculator extends plugin {
       priority: 0,
       rule: [
         {
-          reg: new RegExp(`^${reg}(.*)(养成|计算)+\\s*([0-9,， .\\-|]*)$`, 'i'),
+          reg: new RegExp(`^${reg}(.*)(养成|计算)+\\s*([0-9a-c,， .\\-|]*)$`, 'i'),
           fnc: 'Calculator',
           log: false
         }
@@ -24,7 +24,7 @@ export class calculator extends plugin {
 
   /** 养成计算前置处理 */
   async Calculator () {
-    const match = this.e.msg?.match(new RegExp(`^${reg.replace(/\(/g, '(?:')}([^${reg}]+?)\\s*(养成|计算)+\\s*([0-9,， .\\-|]*)$`))
+    const match = this.e.msg?.match(new RegExp(`^${reg.replace(/\(/g, '(?:')}([^${reg}]+?)\\s*(养成|计算)+\\s*([0-9a-z,， .\\-|]*)$`,'i'))
 
     const name = match?.[1]?.replace?.(/养成|计算/g, ' ')?.trim?.()
     if (!name) {
@@ -37,38 +37,55 @@ export class calculator extends plugin {
     const calculator = {
       roles: [],
       weapons: [],
-      set: sets.map(i => _.compact(i.split(',') ?? []).map(v => v.split('-').map(Number)))
+      set: sets.map(i => _.compact(i.split(',') ?? []).map(v => v.split('-')))
     }
 
     let game = ''
     const roles = []
     const weapons = []
-    for (const name of names) {
-      const role = Character.get(name)
+    let next = false
+    for (const idx in names) {
+      if (next) {
+        next = false
+        continue
+      }
+      const role = Character.get(names[idx])
       if (!role) {
+        roles.push('')
         MysUtil.eachGame((game) => {
-          const w = Weapon.get(name, game)
-          if (w) weapons.push(w)
+          const w = Weapon.get(names[idx], game)
+          weapons.push(w || '')
         })
       } else {
         if (!game) game = role.game
         roles.push(role)
+        const w = Weapon.get(names[Number(idx) + 1], game)
+        if (w) next = true
+        weapons.push(w || '')
       }
     }
 
     const useNames = []
     if (roles.length > 0) {
-      for (const role of roles) {
-        if (role.game !== game) continue
-
-        const weapon = weapons.find(w => w.game === game && role.weaponType === w.type)
-        calculator.roles.push([role, weapon])
-        _.pull(weapons, weapon)
-      }
-      calculator.roles = calculator.roles.slice(0, 8)
-      calculator.roles.forEach(([role, weapon]) => {
-        useNames.push((role?.name || '') + '-' + (weapon?.name || ''))
+      const useWeapon = []
+      roles.forEach((role, idx) => {
+        if (role?.game === game) {
+          useWeapon.push(weapons[idx])
+          calculator.roles.push([role, weapons[idx]])
+        }
       })
+      _.pull(weapons, ...useWeapon)
+      _.compact(weapons)
+
+      calculator.roles = calculator.roles.slice(0, 8)
+      for (const i in calculator.roles) {
+        let [role, weapon] = calculator.roles[i]
+        if (!weapon && weapons.length > 0) {
+          weapon = weapons[0]
+          delete weapons[0]
+        }
+        useNames.push((role?.name || '') + '-' + (weapon?.name || ''))
+      }
     } else if (weapons.length > 0) {
       game = weapons[0]?.game
       calculator.weapons = (weapons.filter(w => w.game === game).map(w => ['', w])).slice(0, 8)
